e4fdd70af69df3c39b7bd226d82f8bbb3c7c1a36
VMaina2003 <victormaina05@gmail.com>
2025-07-10 16:42:47 +0300

    Add the API Functions

---
+++ script.js
@@ -0,0 +1,5 @@
+const API_KEY = ""; 
+const BASE_URL = "https://api.themoviedb.org/3";
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
+const ORIGINAL_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original"; 
+
